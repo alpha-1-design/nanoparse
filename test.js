d9819a8845957509d679fe4b3b209a1685d18ea1
alpha-1-design <alphariansamuel@gmail.com>
2026-04-25 02:50:02 +0000

    Fix import spacing and add EOF newline

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 import assert from 'node:assert';
-import { test, describe } from 'node:test';
+import {test, describe} from 'node:test';
 import nanoparse from './index.js';
 
 describe('nanoparse', () => {
